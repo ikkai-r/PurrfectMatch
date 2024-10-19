7d0c4458ac3faca54f63ce2fec688273baed0b2d
ikkai-r <rica_sales@dlsu.edu.ph>
2024-10-19 22:58:11 +0800

    added schedule and reject/accept apps

--- app/src/main/java/com/example/purrfectmatch/ViewApplication.java
+++ app/src/main/java/com/example/purrfectmatch/ViewApplication.java
@@ -1,6 +1,10 @@
 package com.example.purrfectmatch;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.app.Dialog;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ViewApplication extends AppCompatActivity {
+
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,39 @@ public class ViewApplication extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_reject_app);
+
+        Button rejectButton = dialog.findViewById(R.id.dialog_reject_button);
+        Button cancelButton = dialog.findViewById(R.id.dialog_cancel_button);
+
+        rejectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Perform the delete action here
+                Intent i = new Intent(ViewApplication.this, ShelterPage.class);
+                // Close the dialog
+                ViewApplication.this.startActivity(i);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Just dismiss the dialog
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+    public void rejectApp(View v) {
+        dialog.show();
+    }
+
+    public void acceptApp(View v) {
+        Intent i = new Intent(this, ScheduleShelter.class);
+        this.startActivity(i);
     }
 }
