49ad510bdcd2604ab37e10425c009633334c7016
China Ortiz <ma_china_ortiz@dlsu.edu.ph>
2024-10-19 21:41:10 +0800

    add view application & fix arrow issue in register

--- app/src/main/java/com/example/purrfectmatch/ViewApplication.java
+++ app/src/main/java/com/example/purrfectmatch/ViewApplication.java
@@ -1,8 +1,6 @@
 package com.example.purrfectmatch;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,37 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ShelterPage extends AppCompatActivity {
+public class ViewApplication extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_shelter_page);
+        setContentView(R.layout.activity_view_application);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void goPendingApps(View v) {
-        Intent i = new Intent(this, PendingApplications.class);
-        this.startActivity(i);
-    }
-
-    public void goScheduledApp(View v) {
-        Intent i = new Intent(this, ScheduledApplications.class);
-        this.startActivity(i);
-    }
-
-    public void addCat(View v) {
-        Intent i = new Intent(this, AddCat.class);
-        this.startActivity(i);
-    }
-
-    public void goViewApplication(View v) {
-        Intent i = new Intent(this, ViewApplication.class);
-        this.startActivity(i);
     }
 }
