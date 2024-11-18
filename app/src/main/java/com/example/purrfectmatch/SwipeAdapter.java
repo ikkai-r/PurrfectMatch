3a5dab0468da18054598b8c274acd18e38f4e914
China Ortiz <ma_china_ortiz@dlsu.edu.ph>
2024-11-19 02:07:54 +0800

    change cat profile look

--- app/src/main/java/com/example/purrfectmatch/SwipeAdapter.java
+++ app/src/main/java/com/example/purrfectmatch/SwipeAdapter.java
@@ -15,107 +15,99 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
+import android.graphics.PorterDuff;
 
 public class SwipeAdapter extends RecyclerView.Adapter<SwipeAdapter.ViewHolder>{
 
 
-    SwipeData[] catDataItemArrayList;
-    Context context;
-    boolean flip;
-    int imageIndex = 0;
+SwipeData[] catDataItemArrayList;
+Context context;
+boolean flip;
+int imageIndex = 0;
 
-    public SwipeAdapter(SwipeData[] catDataItemArrayList, SwipeActivity activity) {
-        this.catDataItemArrayList = catDataItemArrayList;
-        this.context = activity;
+public SwipeAdapter(SwipeData[] catDataItemArrayList, SwipeActivity activity) {
+    this.catDataItemArrayList = catDataItemArrayList;
+    this.context = activity;
+}
+
+@NonNull
+@Override
+public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_item, parent, false);
+
+    return new ViewHolder(view);
+}
+
+@Override
+public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    SwipeData swipeDataItem = catDataItemArrayList[position];
+
+    holder.catImage.setImageResource(swipeDataItem.catImages[0]);
+    holder.ageText.setText(String.valueOf(swipeDataItem.age) + "months");
+    holder.weightText.setText(String.valueOf(swipeDataItem.weight) + "lbs");
+    holder.sexText.setText(String.valueOf(swipeDataItem.sex));
+    holder.breedText.setText(swipeDataItem.breed);
+    holder.birthdayText.setText(swipeDataItem.birthday);
+    holder.temperamentText.setText(swipeDataItem.temperament);
+    holder.bioText.setText(swipeDataItem.bio);
+    holder.medicalHistoryText.setText(swipeDataItem.medicalHistory);
+    holder.adoptionFeeText.setText(String.valueOf(swipeDataItem.adoptionFee));
+    holder.contactInformationText.setText(swipeDataItem.contactInformation);
+    holder.nameText.setText(swipeDataItem.name);
+
+    if (flip) {
+        holder.scrollView.setRotationY(180);
+    } else {
+        // Reset rotation if not flipped
+        holder.scrollView.setRotationY(0);
     }
 
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_item, parent, false);
-
-        return new ViewHolder(view);
+    if (swipeDataItem.isBookmarked) {
+        holder.bookmarkIcon.setColorFilter(0xFFCE0000, PorterDuff.Mode.SRC_IN);
+    } else {
+        holder.bookmarkIcon.setColorFilter(0xFF808080, PorterDuff.Mode.SRC_IN); 
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+}
 
-        SwipeData swipeDataItem = catDataItemArrayList[position];
+public void setFlip(boolean b){
+    this.flip = b;
+}
 
-        holder.catImage.setImageResource(swipeDataItem.catImages[0]);
+@Override
+public int getItemCount() {
+    return catDataItemArrayList.length;
+}
 
-        holder.catImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                imageIndex = (imageIndex + 1) % swipeDataItem.catImages.length;
-                holder.catImage.setImageResource(swipeDataItem.catImages[imageIndex]);
-            }
-        });
+public class ViewHolder extends RecyclerView.ViewHolder{
 
-        holder.neuteredImage.setImageResource(swipeDataItem.neuteredImage);
-        holder.vaccinatedImage.setImageResource(swipeDataItem.vaccinationImage);
-        holder.litterTrainedImage.setImageResource(swipeDataItem.litterTrainedImage);
-
-        holder.ageText.setText(String.valueOf(swipeDataItem.age) + "months");
-        holder.weightText.setText(String.valueOf(swipeDataItem.weight) + "lbs");
-        holder.sexText.setText(String.valueOf(swipeDataItem.sex));
-        holder.breedText.setText(swipeDataItem.breed);
-        holder.birthdayText.setText(swipeDataItem.birthday);
-        holder.temperamentText.setText(swipeDataItem.temperament);
-        holder.bioText.setText(swipeDataItem.bio);
-        holder.medicalHistoryText.setText(swipeDataItem.medicalHistory);
-        holder.adoptionFeeText.setText(String.valueOf(swipeDataItem.adoptionFee));
-        holder.contactInformationText.setText(swipeDataItem.contactInformation);
-        holder.nameText.setText(swipeDataItem.name);
-
-        if (flip) {
-            holder.scrollView.setRotationY(180);
-        } else {
-            // Reset rotation if not flipped
-            holder.scrollView.setRotationY(0);
-        }
+    ImageView catImage, bookmarkIcon;
+    TextView ageText, weightText, sexText, breedText, birthdayText, temperamentText, bioText, medicalHistoryText, adoptionFeeText, contactInformationText, nameText;
+    ScrollView scrollView;
 
 
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        catImage = itemView.findViewById(R.id.catPic);
+        
+        ageText = itemView.findViewById(R.id.ageText);
+        weightText = itemView.findViewById(R.id.weightText);
+        sexText = itemView.findViewById(R.id.sexText);
+        breedText = itemView.findViewById(R.id.breedText);
+        birthdayText = itemView.findViewById(R.id.birthdayText);
+        temperamentText = itemView.findViewById(R.id.tempermentText);
+        bioText = itemView.findViewById(R.id.bioText);
+        medicalHistoryText = itemView.findViewById(R.id.medicalHistoryText);
+        adoptionFeeText = itemView.findViewById(R.id.adoptionFeeText);
+        contactInformationText = itemView.findViewById(R.id.contactInformationText);
+        nameText = itemView.findViewById(R.id.nameText);
+
+        scrollView = itemView.findViewById(R.id.scrollView2);
+        bookmarkIcon = itemView.findViewById(R.id.bookmarkIcon); 
     }
 
-    public void setFlip(boolean b){
-        this.flip = b;
-    }
-
-    @Override
-    public int getItemCount() {
-        return catDataItemArrayList.length;
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder{
-
-        ImageView catImage, neuteredImage, vaccinatedImage, litterTrainedImage;
-        TextView ageText, weightText, sexText, breedText, birthdayText, temperamentText, bioText, medicalHistoryText, adoptionFeeText, contactInformationText, nameText;
-        ScrollView scrollView;
-
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            catImage = itemView.findViewById(R.id.catPic);
-            neuteredImage = itemView.findViewById(R.id.neuteredImageView);
-            vaccinatedImage = itemView.findViewById(R.id.vaccinatedImageView);
-            litterTrainedImage = itemView.findViewById(R.id.litterTrainedImageView);
-
-            ageText = itemView.findViewById(R.id.ageText);
-            weightText = itemView.findViewById(R.id.weightText);
-            sexText = itemView.findViewById(R.id.sexText);
-            breedText = itemView.findViewById(R.id.breedText);
-            birthdayText = itemView.findViewById(R.id.birthdayText);
-            temperamentText = itemView.findViewById(R.id.tempermentText);
-            bioText = itemView.findViewById(R.id.bioText);
-            medicalHistoryText = itemView.findViewById(R.id.medicalHistoryText);
-            adoptionFeeText = itemView.findViewById(R.id.adoptionFeeText);
-            contactInformationText = itemView.findViewById(R.id.contactInformationText);
-            nameText = itemView.findViewById(R.id.nameText);
-
-            scrollView = itemView.findViewById(R.id.scrollView2);
-        }
-
-    }
+}
 }
