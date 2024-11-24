7c790c30ba2f30a005f548c5a28800682e6a528c
Rica Mae Sales <92283086+ikkai-r@users.noreply.github.com>
2024-11-24 21:32:09 +0800

    user profile pulls from db, TODO: edit profile

--- app/src/main/java/com/example/purrfectmatch/ProfileActivity.java
+++ app/src/main/java/com/example/purrfectmatch/ProfileActivity.java
@@ -2,8 +2,13 @@ package com.example.purrfectmatch;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,7 +18,8 @@ import org.w3c.dom.Text;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView applications;
+    TextView applications, bioText, locationProfile, nameProfile, phoneNumberProfile, lifestyleText;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +31,16 @@ public class ProfileActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.profile);
 
+        mAuth = FirebaseAuth.getInstance();
         applications = findViewById(R.id.textView22);
         logoutBtn = findViewById(R.id.logoutBtn);
+        bioText = findViewById(R.id.bioTextProfile);
+        locationProfile = findViewById(R.id.locationProfile);
+        nameProfile = findViewById(R.id.nameProfile);
+        phoneNumberProfile = findViewById(R.id.phoneNumberProfile);
+        lifestyleText = findViewById(R.id.lifestyleText);
 
+        setUserInfo();
 
         applications.setOnClickListener(view -> {
             Intent i = new Intent(this, ApplicationsActivity.class);
@@ -39,6 +52,7 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
         explore = findViewById(R.id.imageView19);
+
 
         swipe = findViewById(R.id.imageView17);
 
@@ -63,9 +77,51 @@ public class ProfileActivity extends AppCompatActivity {
         finish();
     }
 
+    private void setUserInfo() {
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference userRef = db.collection("Users").document(user.getUid());
+
+            // Fetch data from Firestore using the reference
+            userRef.get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            // Extract data from the document snapshot
+                            String nameText = documentSnapshot.getString("firstName") + " " + documentSnapshot.getString("lastName");
+
+                            String lifestylePref = "Household Members: " + documentSnapshot.getString("householdMembers") + "\n" +
+                                    "Other pets: " + documentSnapshot.getString("otherPets") + "\n" +
+                                    "Cat Preferences: " + documentSnapshot.getString("preferences1") + ", " + documentSnapshot.getString("preferences2");
+                            String phoneNumberTxt = documentSnapshot.getString("phoneNumber");
+                            String bioTxt = documentSnapshot.getString("bio");
+                            String locationTxt = documentSnapshot.getString("city") + ", " + documentSnapshot.get("province") + ", " + documentSnapshot.get("country");
+
+                            // Display the data in the UI
+                            nameProfile.setText(nameText);
+                            lifestyleText.setText(lifestylePref);
+                            phoneNumberProfile.setText(phoneNumberTxt);
+                            bioText.setText(bioTxt);
+                            locationProfile.setText(locationTxt);
+                        } else {
+                           Log.d("fe", "user doesnt exist");
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        Log.d("fe", "failed to get info");
+                    });
+        } else {
+            Log.d("fe", "user is not signed in");
+        }
+    }
+
+
     @Override
     public void onBackPressed() {
         // Do nothing, so back navigation is disabled
+        super.onBackPressed();
     }
 
 }
+
